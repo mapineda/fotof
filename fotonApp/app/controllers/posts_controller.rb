6cbb2717311ad2a2a946e54ac74cac7f4fcc5e35
mapineda <mapineda1222@gmail.com>
2016-04-01 18:39:55 -0500

    adding new view and adding it to post_controller

--- fotonApp/app/controllers/posts_controller.rb
+++ fotonApp/app/controllers/posts_controller.rb
@@ -4,6 +4,17 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :caption)
   end
 
 end
