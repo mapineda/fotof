9b050aef701af2d288b9216e23da28139420e6ce
mapineda <mapineda1222@gmail.com>
2016-04-03 12:36:18 -0500

    added post.all to index in post controller

--- fotonApp/app/controllers/posts_controller.rb
+++ fotonApp/app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
   def index
+    @post = Post.all
   end
 
   def new
@@ -9,7 +10,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create(post_params)
-    redirect_to posts_path 
+    redirect_to posts_path
   end
 
   private
--- fotonApp/app/models/post.rb
+++ fotonApp/app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ActiveRecord::Base
   has_attached_file :image, styles: { :medium => "640px"}
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
-  
+  post = Post.first
+
+
 
 end
